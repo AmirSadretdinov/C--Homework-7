1e0647ed74c67bb762ab33f36e020d48fd1d24b9
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-20 14:51:17 +0300

    Ready

---
+++ Ex47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] Create2DArray(int m, int n)
+{
+    double[,] array = new double[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(-3,4) / 10.0;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] res = Create2DArray(3,4);
+Print2DArray(res);
---
+++ Ex50/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число и ищет в двумерном массиве, 
+// и возвращает индексы этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 7 -> 0 , 2
+// 5 -> 1 , 0
+// 18 -> нет такого элемента
+int[,] Create2DArray(int m, int n)
+{
+    int[,] array = new int[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(1,10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string FindElem(int[,] array, int find)
+{
+    string res = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (find == array[i,j])
+            {
+                res = $"{i},{j}";
+                break;
+            }
+        }
+    }
+    return res;
+}
+
+int[,] res = Create2DArray(3,4);
+Print2DArray(res);
+string str = FindElem(res,5);
+System.Console.WriteLine(str);
